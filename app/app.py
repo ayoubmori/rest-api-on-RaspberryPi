615bebb95900b84265aea04c3fc497b3b2728bff
ayoubmori <ayoubmori33@gmail.com>
2024-05-14 03:36:09 +0100

    clean app.py

--- app/app.py
+++ app/app.py
@@ -2,15 +2,6 @@ from fastapi import FastAPI
 from app.controllers.relay_routes import relay_route 
 from app.controllers.dgip_routes import degitalinput
 
-# from fastapi import HTTPException
-# from fastapi.responses import JSONResponse
-# from fastapi.requests import Request
-# from datetime import datetime
-
-# from fastapi import FastAPI, Response, HTTPException
-# from datetime import datetime
-# from typing import List, Optional
-# from pydantic import BaseModel
 
 app = FastAPI(
     title="rest api",
